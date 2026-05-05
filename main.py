7974555fcff3544f4c7755b86feab19afe517b87
Sebastian Aguiar <sebastianaguiar9511@gmail.com>
2026-05-04 23:15:25 -0500

    main

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from bank_account import BankAccount
+from person import Person
+from utils import person_data, balance_summary
+
 def main():
     people = []  # List to store all Person objects
 
@@ -13,15 +17,33 @@ def main():
 
         # Option 1: Add a new person
         if choice == "1":
-            pass
+            nueva_persona = person_data()
+            people.append(nueva_persona)
 
         # Option 2: Add an account to an existing person
         elif choice == "2":
-            pass
+            name_to_find = input("Enter the person's name:\n")
+            found_person = None
+            for p in people:
+                if p.name == name_to_find:
+                    found_person = p
+                    break
+            
+            if found_person:
+                acc_num = int(input("Enter a 4-digit account number:\n"))
+                balance = float(input("Enter the initial balance:\n"))
+                
+                new_acc = BankAccount(acc_num, balance)
+                found_person.add_account(new_acc)
+            else:
+                print("Person not found.")
 
         # Option 3: Show all balances
         elif choice == "3":
-            pass
+            if not people:
+                print("No data to show.")
+            else:
+                balance_summary(people)
 
         # Option 4: Quit
         elif choice == "4":
